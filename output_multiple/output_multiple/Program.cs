69f2ed69a27cd242e33e74c224814b3989e430da
madajayasree <93071345+madajayasree@users.noreply.github.com>
2021-10-29 10:17:46 +0530

    Add files via upload

---
+++ output_multiple/output_multiple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace output_multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name, age, experience, job and Qualification ");
+
+            string details = Console.ReadLine();
+            String[] allDetails = details.Split(','); 
+                                
+          
+            foreach (var k in allDetails)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
